a8f4f89320842bb364bc57c65c9afbe4bfffacd6
Alan Deng <72290296+Al-nD@users.noreply.github.com>
2024-10-02 23:27:33 -0400

    Added fps coice
    and i also made some final text green

--- fps-unlocker.sh
+++ fps-unlocker.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-fps=90
+GREEN='\033[1;32m'
+
 path="/Applications/Roblox.app"
 settings_path="$path/Contents/MacOS/ClientSettings"
+
+echo "Choose the amount of frames you want"
+read fps
 if [ -d "/Applications/Roblox.app/Contents/MacOS/ClientSettings" ]; then
     echo "The folder exists."
     echo "{\"DFIntTaskSchedulerTargetFps\": $fps }" > "$settings_path/ClientAppSettings.json"
+    echo "$GREEN FPS UNLOCKED${NC}"
 else
     echo "The folder does not exist."
     mkdir -p "/Applications/Roblox.app/Contents/MacOS/ClientSettings"
